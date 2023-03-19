a42770773a603a7f1a4f1006b493a83e6fb316c8
fendou99@126.com <fendou99@126.com>
2023-03-19 20:18:21 +0800

    测试下

--- java-first/src/com/itheima/JavaFirst.java
+++ java-first/src/com/itheima/JavaFirst.java
@@ -3,7 +3,7 @@ package com.itheima;
 public class JavaFirst {
     public static void main(String[] args) {
         System.out.println("11111111111111111111");
-
+        System.out.println("这是测试！");
         System.out.println("first time! ");
     }
 }
