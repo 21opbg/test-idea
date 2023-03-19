ca73b481d968e4f1149b8bb9687fe45e700476cd
fendou99@126.com <fendou99@126.com>
2023-03-19 19:29:11 +0800

    第一次提交内容

--- java-first/src/com/itheima/JavaFirst.java
+++ java-first/src/com/itheima/JavaFirst.java
@@ -3,5 +3,7 @@ package com.itheima;
 public class JavaFirst {
     public static void main(String[] args) {
         System.out.println("11111111111111111111");
+
+        System.out.println("first time! ");
     }
 }
